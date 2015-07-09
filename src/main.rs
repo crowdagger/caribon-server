6ed80af60e8e0049d5672fad37fc6cf629a4dadc
Elisabeth Henry <liz.henry@ouvaton.org>
2015-07-09 04:12:33 +0200

    Now uses Foundation

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,46 @@ use hyper::server::Listening;
 use caribon::Parser;
 use router::Router;
 use std::error::Error;
+use hyper::Client;
+use std::io::Read;
 
 fn main() {
     fn router() -> Router {
         let mut router = Router::new();
         router.get("/", show_form);
+//        router.get("/url", show_url);
         router.get("/style.css", show_css);
         router.get("/serialize.js", show_js);
         router.post("/result", show_result);
+        router.get("/foundation.css", show_foundation_css);
+        router.get("/normalize.css", show_normalize_css);
+        router.get("/modernizr.js", show_modern_js);
+        router.get("/caribon.png", show_logo);
         router
+    }
+
+    // fn show_url(_: &mut Request) -> IronResult<Response> {
+    //     let url = "http://linuxfr.org/";
+    //     let mut html:String = String::new();
+
+    //     let client = Client::new();
+    //     let mut res = client.get(url).send().unwrap();
+
+    //     if res.status != hyper::Ok {
+    //         html = html + "Error fetching URL";
+    //         println!("status:{:?}", res.status);
+    //     } else {
+    //         res.read_to_string(&mut html).unwrap();
+    //     }
+
+    //     let content_type = "text/html; charset=UTF-8".parse::<Mime>().unwrap();
+    //     Ok(Response::with((content_type, status::Ok, html)))
+    // }
+
+    fn show_logo(_: &mut Request) -> IronResult<Response> {
+        let img:&'static[u8] = include_bytes!("html/caribon.png");
+        let content_type = "image/png".parse::<Mime>().unwrap();
+        Ok(Response::with((content_type, status::Ok, img)))
     }
 
     fn show_js(_: &mut Request) -> IronResult<Response> {
@@ -31,9 +62,27 @@ fn main() {
         Ok(Response::with((content_type, status::Ok, js)))
     }
 
+    fn show_modern_js(_: &mut Request) -> IronResult<Response> {
+        let js = include_str!("html/modernizr.js");
+        let content_type = "text/javascript".parse::<Mime>().unwrap();
+        Ok(Response::with((content_type, status::Ok, js)))
+    }
+
     
     fn show_css(_: &mut Request) -> IronResult<Response> {
         let css = include_str!("html/main.css");
+        let content_type = "text/css".parse::<Mime>().unwrap();
+        Ok(Response::with((content_type, status::Ok, css)))
+    }
+
+    fn show_foundation_css(_: &mut Request) -> IronResult<Response> {
+        let css = include_str!("html/foundation.min.css");
+        let content_type = "text/css".parse::<Mime>().unwrap();
+        Ok(Response::with((content_type, status::Ok, css)))
+    }
+
+    fn show_normalize_css(_: &mut Request) -> IronResult<Response> {
+        let css = include_str!("html/normalize.css");
         let content_type = "text/css".parse::<Mime>().unwrap();
         Ok(Response::with((content_type, status::Ok, css)))
     }
